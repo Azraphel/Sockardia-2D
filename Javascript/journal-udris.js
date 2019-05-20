a4bc3d06bade486539b5f4ce3a35b34f67618e70
Azraphel <stephanedeffoe@hotmail.fr>
2019-05-20 17:16:00 -0300

    Le système pour tourner les pages marchent complétement. Il n'y a plus de bug lorsque les pages sont tourné de droite à gauche.

--- Javascript/journal-udris.js
+++ Javascript/journal-udris.js
@@ -1,79 +1,76 @@
+var present_page_recto;
+
 function TournerLesPages(page_cliquer) {
-    
+
     if (document.getElementsByClassName("page-qui-tourne").length > 0) {
         return;
     }
 
-    if(page_cliquer.classList.contains("recto")) {
-
-
-        console.log("c'est une page recto qui a ete toucher");
-
-        var feuille = page_cliquer.parentElement;
-        //feuille.classList.remove("page-retourner");        
-        feuille.classList.add("page-tourner", "page-qui-tourne");
-        
-        setTimeout(function() {
-
-            feuille.classList.remove("page-qui-tourne");
-        
-        }, 2000);
-        
-        if (feuille.nextElementSibling != null) {
-
-            setTimeout(function() {
-                feuille.nextElementSibling.classList.remove("page-en-cours-de-lecture");
-                feuille.classList.add("page-en-cours-de-lecture");
-            }, 2000);
-        }
-
-    }
-    else {
-
-        /*
-            Le probl'eme semble se trouver dans la durer pour la transition
-            on peut chercher un moyen de donner la dur/ de transition de 0
-            mais même à cela. Il y a quelques chose qui ne fait pas de sens
-            dans cette affaire là. Pourquoi seulement lorsque tout se trouve
-            dans le timeout que ça marche?
-        */
-
-        console.log("c'est une page verso qui a ete toucher");
-
-        var feuille = page_cliquer.parentElement;
-
-        feuille.classList.add("page-qui-tourne");
-        //feuille.classList.remove("page-en-cours-de-lecture");
-        
-        
-        setTimeout(function() {
-            feuille.classList.remove("page-tourner", "page-qui-tourne");
-
-            feuille.nextElementSibling.classList.remove("page-qui-tourne");
-            
-            if (feuille.nextElementSibling != null) {
     
-                feuille.nextElementSibling.classList.add("page-en-cours-de-lecture");
+    if (page_cliquer.classList.contains("recto")) {
+        
+        console.log("recto");
+        
+        present_page_recto.classList.add("page-qui-tourne");
+        present_page_recto.classList.remove("recto");
+        present_page_recto.classList.add("verso");
+        
+        setTimeout(function () {
+            
+            present_page_recto.classList.remove("page-qui-tourne");
+            present_page_recto.classList.add("page-en-cours-de-lecture");
+            
+            if (present_page_recto.nextElementSibling != null) {
+                present_page_recto.nextElementSibling.classList.remove("page-en-cours-de-lecture");
             }
 
-        }, 2000);
+            if (present_page_recto.previousElementSibling != null) {
+                present_page_recto = present_page_recto.previousElementSibling;
+            }
+            
+        }, 1000);
 
     }
+
+    else {
+
+        console.log("verso");
+        
+        if (present_page_recto.classList.contains("recto")) {
+            present_page_recto = present_page_recto.nextElementSibling;
+        }
+        
+        present_page_recto.classList.remove("verso");
+        present_page_recto.classList.add("page-qui-tourne", "recto");
+        
+        if (present_page_recto.nextElementSibling != null) {
+            present_page_recto.nextElementSibling.classList.add("page-en-cours-de-lecture");
+        }
+        
+        present_page_recto.classList.remove("page-en-cours-de-lecture");
+        
+        setTimeout(function () {
+            
+            present_page_recto.classList.remove("page-qui-tourne");
+        
+        }, 500);
+
+    }
+    
 
 }
 
 function RajouterEvenement() {
-    var pages_recto = document.getElementsByClassName("recto");
-    var pages_verso = document.getElementsByClassName("verso");
+    var pages = document.getElementsByClassName("page");
     
-    for (var i = 0; i < pages_recto.length; i++) {
-        page_recto = pages_recto[i];
-        page_recto.addEventListener("click", function() { TournerLesPages(this) });
-    }
+    for (var i = 0; i < pages.length; i++) {
 
-    for (var i = 0; i < pages_verso.length; i++) {
-        page_verso = pages_verso[i];
-        page_verso.addEventListener("click", function() { TournerLesPages(this) });
+        if (i == pages.length - 1) {
+            present_page_recto = pages[i];
+        }
+
+        pages[i].classList.add("recto");
+        pages[i].addEventListener("click", function() { TournerLesPages(this) });
     }
 }
 
